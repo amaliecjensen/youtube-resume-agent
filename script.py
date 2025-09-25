8f5a1a5cfa9e0cd2cfd1214392b1d141aec70c58
amaliecjensen <amaliecolstrup@icloud.com>
2025-09-25 15:42:18 +0200

    youtube url transcript function

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from youtube_transcript_api import YouTubeTranscriptApi
+import re
 
 #lav en rag der giver et resume af en youtube video
 
@@ -14,10 +16,26 @@ chat = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo", temperature=0)
 
 
 # Chat loop
-while True:
-    question = input("\nAsk about youtube content (or 'quit'): ")
-    if question.lower() == 'quit':
-        break
-    answer = xxxx(question)
-    print(f"Answer: {answer}")
+#while True:
+  #  question = input("\nInsert Youtube URL to generate content(or 'quit'): ")
+   # if question.lower() == 'quit':
+       # break
+   # answer = xxxx(question)
+  #  print(f"Answer: {answer}")
 
+def yt_transcript(url):
+    match = re.search(r'(?:v=|\/)([0-9A-Za-z_-]{11})', url)
+
+    if not match:
+        return "could not find video id in url"
+    
+    videoId= match.group(1) # hent det fundne ID
+
+    api = YouTubeTranscriptApi()
+    result = api.fetch(videoId)
+
+    text = " ".join([snippet.text for snippet in result.snippets])  # ✅ Virker!
+    print(text)
+    return text
+
+yt_transcript("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
